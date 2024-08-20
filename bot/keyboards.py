9b34ff5f77afb3d6c8f8de258c51b3b2ed04d661
Yehor Demenchuk <demenchuk1210m@gmail.com>
2024-08-20 14:41:05 +0200

    feat(parser-module): create functions to parse vacancy sites

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,3 +1,8 @@
+# MyProject - A telegram bot for finding jod in Slovakia.
+# © 2024 Yehor Demenchuk. All rights reserved.
+# Contact: demenchuk1210m@gmail.com
+# This code is provided "as is", without warranty of any kind.
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 main_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='hladať')]], resize_keyboard=True)
